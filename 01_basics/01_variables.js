86bdab00ceb03f92b8a52a565fe7deaabbb21981
Roushan Thapa <166424218+rOUSHAN084@users.noreply.github.com>
2025-04-12 15:07:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "roushan2003@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountID = 2
+
+accountEmail= "hc@gnail.com"
+accountPassword = "21342"
+accountCity = "bangluru"
+
+console.log(accountId);
+
+
+/*
+prefer not to use var
+*/
+
+
+console.table([accountId,accountEmail,accountCity,accountPassword, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newr version
+
+//alert(3 +3)
+
+console.log(3+3)
+console.log()
+
+
+let name = "roushan"
+let age = 17
+let isLoggedIn = true
+
+// Number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqe
+
+//object
+
+console.log(typeof undefined);
