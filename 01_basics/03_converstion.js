d4591484c2da8b417d4558b8c8db1ed0a4139fa3
Roushan Thapa <166424218+rOUSHAN084@users.noreply.github.com>
2025-04-13 03:14:21 +0000

    converstions

---
+++ 01_basics/03_converstion.js
@@ -0,0 +1,28 @@
+let score = "40abc"
+
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+let someNumbers = 40
+
+let stringNumber = String(someNumbers)
+
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
